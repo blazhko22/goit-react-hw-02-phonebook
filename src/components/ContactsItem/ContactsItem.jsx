49bbf814401f99acf6ccaaf0ade3dff8c9fa4916
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2022-02-14 13:51:19 +0200

    DZ2.21

---
+++ src/components/ContactsItem/ContactsItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import s from './ContactsItem.module.css';
+
+function ContactsItem({
+  contact: { name, number, id },
+  onRemoveContact,
+}) {
+  return (
+    <li className={s.item}>
+      <span>
+        {name}: {number}
+      </span>
+      <button
+        type="button"
+        data-id={id}
+        onClick={e => {
+          onRemoveContact(e.target.dataset.id);
+        }}
+      >
+        delete
+      </button>
+    </li>
+  );
+}
+
+export default ContactsItem;
